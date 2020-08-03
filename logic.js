a029612cc1803f860ac0659c17d1926418333241
elementalblockchain <69003738+elementalblockchain@users.noreply.github.com>
2020-08-03 11:03:09 -0700

    making table

--- logic.js
+++ logic.js
@@ -13,7 +13,22 @@ $(document).ready(function(){
       // need to iterate through the array and make a list element for each coin
         var list = document.getElementById('coinlist');
         for (i = 0; i < 100; i++) {
-          var entry = document.createElement('li');
+          var entry = document.createElement('tr');
+          name = data[i].name
+          mcap = data[i].market_cap
+          price = data[i].current_price
+          vol24 = data[i].total_volume
+          supply = data[i].circulating_supply
+          change24 = data[i].price_change_percentage_24h
+
+          values = [name, mcap, price, vol24, supply, change24]
+          for (i = 0; i <= values.length; i++) {
+            var entry = document.createElement('td');
+            entry.innerText = value[i]
+            row.appendChild(entry)
+          });
+
+          entry.innerText = name
           list.appendChild(entry);
         }
     });
