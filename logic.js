f21233bec80aa38d6c27064f25aeea734d61e136
elementalblockchain <69003738+elementalblockchain@users.noreply.github.com>
2020-08-04 08:03:08 -0700

    added sort function
    sort func copied from w3schools site

--- logic.js
+++ logic.js
@@ -1,4 +1,37 @@
-
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("coinlist");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      // Check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        // If so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 
 $(document).ready(function(){
   let BASE_URL = "https://api.coingecko.com/api/v3"
@@ -42,10 +75,8 @@ $(document).ready(function(){
           cell5.innerHTML = vol24;
           cell6.innerHTML = supply;
           cell7.innerHTML = change24;
-
-
         }
-
+      });
     });
-  });
+
 });
